216a97a347c47a27a33d9db1b74e953770cd928c
Justin Mancinelli <justin.mancinelli@gmail.com>
2015-01-01 21:15:33 +0900

    fix whitespace

--- js/app.js
+++ js/app.js
@@ -4,6 +4,6 @@ var SUMMARY = "Just a little blurb about me";
 var AUTHOR = "My name goes here";
 
 React.render(
-  <Footer title={TITLE} summary={SUMMARY} author={AUTHOR}/>,
+  <Footer title={TITLE} summary={SUMMARY} author={AUTHOR} />,
   document.getElementById('example')
 );
