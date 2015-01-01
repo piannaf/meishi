e647d19a86caa7d54fe949cc587bd3db65a5969d
Justin Mancinelli <justin.mancinelli@gmail.com>
2015-01-01 21:15:13 +0900

    include author component for footer

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 // Requires footer.js, a reactjs component
 var TITLE = "Hello, world!";
 var SUMMARY = "Just a little blurb about me";
+var AUTHOR = "My name goes here";
 
 React.render(
-  <Footer title={TITLE} summary={SUMMARY} />,
+  <Footer title={TITLE} summary={SUMMARY} author={AUTHOR}/>,
   document.getElementById('example')
 );
