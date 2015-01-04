37da1be546fdef7abc934a6eb2492f8968cd597d
Justin Mancinelli <justin.mancinelli@gmail.com>
2015-01-04 12:58:34 +0900

    add other social link components
    * Instagram
    * Facebook
    * Twitter
    * Email

--- js/social.js
+++ js/social.js
@@ -5,3 +5,59 @@ var Flickr = React.createClass({
     );
   }
 });
+
+var Instagram = React.createClass({
+  render: function() {
+    return (
+      <a href={this.props.children}>Instagram</a>
+    );
+  }
+});
+
+var Facebook = React.createClass({
+  render: function() {
+    return (
+      <a href={this.props.children}>Facebook</a>
+    );
+  }
+});
+
+var Twitter = React.createClass({
+  render: function() {
+    return (
+      <a href={this.props.children}>Twitter</a>
+    );
+  }
+});
+
+var Email = React.createClass({
+  render: function() {
+    return (
+      <a href={this.props.children}>Email</a>
+    );
+  }
+});
+
+var Social = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <Flickr>
+          {this.props.flickrUrl}
+        </Flickr>
+        <Instagram>
+          {this.props.instagramUrl}
+        </Instagram>
+        <Facebook>
+          {this.props.facebookUrl}
+        </Facebook>
+        <Twitter>
+          {this.props.twitterUrl}
+        </Twitter>
+        <Email>
+          mailto:{this.props.emailAddress}
+        </Email>
+      </div>
+    );
+  }
+});
